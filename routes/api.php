41387282a43823c988d3912128839aab6e817bdf
Jozef Hudec <jojohudec27@gmail.com>
2024-05-25 11:50:57 +0200

    Add register, login, logout functionality

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\LogoutController;
+use App\Http\Controllers\API\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware(['auth:sanctum'])
+    ->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::post('/logout', LogoutController::class);
+    });
+
+Route::post('/login', LoginController::class);
+Route::post('/register', RegisterController::class);

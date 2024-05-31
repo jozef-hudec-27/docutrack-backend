2da6b6a4c9c49199f41b6fb3eee47cc811f62d75
Jozef Hudec <jojohudec27@gmail.com>
2024-05-31 08:49:29 +0200

    Register new routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\LogoutController;
 use App\Http\Controllers\API\Auth\RegisterController;
+use App\Http\Controllers\API\DocumentController;
+use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,8 +13,21 @@ Route::middleware(['auth:sanctum'])
         Route::get('/user', function (Request $request) {
             return $request->user();
         });
-        Route::post('/logout', LogoutController::class);
+        Route::post('/logout', [LogoutController::class, 'delete']);
+        Route::post('/logout-all', [LogoutController::class, 'deleteAll']);
+
+        Route::get('/documents', [DocumentController::class, 'index']);
+        Route::post('/documents', [DocumentController::class, 'store']);
+
+        Route::get("/files/{file_path}", FileController::class);
     });
 
 Route::post('/login', LoginController::class);
 Route::post('/register', RegisterController::class);
+
+// display not found message for all other routes
+Route::fallback(function () {
+    return response()->json([
+        'message' => 'Not Found',
+    ], 404);
+});

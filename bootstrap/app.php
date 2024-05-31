5b3b1432695e69b0e1cad5dd2a80b175c23a1c89
Jozef Hudec <jojohudec27@gmail.com>
2024-05-31 08:38:52 +0200

    Disable CSRF tokens for API routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-        //
+        $middleware->validateCsrfTokens(except: [
+            env('APP_URL', 'http://localhost:8000') . '/api/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
